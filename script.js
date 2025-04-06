78f5f9a7193dff37cdbee60cb237b471a2edf77b
NicolyFernandees <nycolysennaa@gmail.com>
2025-04-05 22:38:04 -0300

    Site de namoro toxico
    botão fugindo

---
+++ script.js
@@ -0,0 +1,21 @@
+function fuja(){
+    var botaoNao = document.getElementById("nao")
+    
+    var largurajanela = window.innerWidth;
+    var alturaJanela = window.innerHeight;
+
+    var maxX = largurajanela - botaoNao.offsetWidth;
+    var maxY = alturaJanela - botaoNao.offsetHeight;
+
+    var aleatorioX = Math.floor(Math.random() * maxX);
+    var aleatorioY = Math.floor(Math.random() * maxY);
+
+    botaoNao.style.left = aleatorioX + "px";
+    botaoNao.style.top = aleatorioY + "px";
+
+}
+
+function parabens(){
+    alert("Parabéns, agora estamos oficialmente namorando, i love you ")
+
+}
